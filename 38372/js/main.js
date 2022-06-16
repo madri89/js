e9626fa31a63cb52fe69b72c9576e47e702265c2
Marcin Rossa <marcinrossa@wp.pl>
2022-06-16 13:33:51 +0200

    zadanie 38372

---
+++ 38372/js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 122;
+let secondNumber = 40;
+
+function sumaliczb(num1, num2){
+    let suma = num1 + num2;
+    return suma;
+}
+
+
+console.log(sumaliczb(firstNumber, secondNumber));
